942cf14bb23f56a7b2e424cd3af3461f4d3cdf86
Kelly Alves <kellyp.alves87@gmail.com>
2020-07-20 15:57:56 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'; //ponto de entrada da aplicação
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Kelly Alves</p>
+      <p>Ama tecnologia</p>
+      <p>E quer ser Frontend</p>
+    </div>
+  );
+}
+
+export default App;
