2ae1a0a914ecfd087f1614a1290a4964fb984867
sjadriana <adriana.sj@live.com>
2020-07-21 17:52:47 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react'; //ponto de entrada da aplicação
+// import React from 'react'; //ponto de entrada da aplicação
+import React, {useState} from 'react'
 import './App.css';
 import Button from './components/Button'
+import Modal from  './components/modal'
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <div className="App">
-      <Button name="Olá" />
-      <p>Bem vindos ao Burguer Queen</p>
+      <button onClick={()=> setIsModalVisible(true)}>Oopen</button>
+      {isModalVisible ? < Modal /> : null}
     </div>
   );
-}
+} 
 
 export default App;
