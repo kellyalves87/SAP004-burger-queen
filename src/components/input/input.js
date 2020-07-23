5f5f53315d1ab2b543c9363d065bc79357e55640
sjadriana <adriana.sj@live.com>
2020-07-22 23:14:21 -0300

    css login

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './Input.css'
+// import './Input.css'
 
 const Input = (props) => {
-  return <input type={props.type} id={props.id} className={props.class} placeholder={props.placeholder}></input>
+  return <input type={props.type} id={props.id} className={props.class} placeholder={props.placeholder} name={props.name}></input>
 }
 
 export default Input;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,10 +4,16 @@ import app from "../base";
 import Image from '../components/image/image';
 import { AuthContext } from "../firebase/Auth";
 import logo from '../assets/logo.png';
+import fries from '../assets/fries.png';
+import juice from '../assets/juice.png';
+import milkshake from '../assets/milkshake.png';
+import soda from '../assets/soda.png';
 import SignUp from "./SignUp";
 import Modal from '../components/modal/modal';
-import Button from '../components/button/Button';
-// import './Login.css';
+import Button from '../components/button/button';
+import Input from '../components/input/Input'
+import './Login.css';
+
 
 const Login = ({ history }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -30,28 +36,32 @@ const Login = ({ history }) => {
   const { currentUser } = useContext(AuthContext);  
 
   return (
-    <div>
-      <header>
+    <div className='div-login' >
       <figure>
-        <Image src={logo} alt='logo' class='logo'/>
+        <Image src={logo} alt='logo' class='logo' />
         </figure>
-      </header>
       <h1>Burger Queen</h1>
       <form onSubmit={handleLogin}>
         <label>      
-          <input name="email" type="email" placeholder="Email" />
+          <Input name="email" type="email" placeholder="Email" />
         </label>
         <label>
-          <input name="password" type="password" placeholder="Senha" />
+          <Input name="password" type="password" placeholder="Senha" />
         </label>
         <Button id="login" class="button-loggin" name="Entrar" type="submit" />
-        <p onClick={ () => setIsModalVisible(true) }>Ainda não tem cadastro? <strong>Registre-se aqui!</strong></p>
+        <p onClick={ () => setIsModalVisible(true) }>Ainda não tem cadastro?<br /> <strong>Registre-se aqui!</strong></p>
         {isModalVisible ? (
-                <Modal onClose={ () => setIsModalVisible(false) }>
-                    <SignUp />
-                </Modal>
-                ) : null}
+          <Modal onClose={ () => setIsModalVisible(false) }>
+          <SignUp />
+          </Modal>
+          ) : null}
       </form>
+      <section className="section">
+        <Image src={fries} alt='logo' class='logo' class='img-section' />
+        <Image src={juice} alt='logo' class='logo' class='img-section' />
+        <Image src={milkshake} alt='logo' class='logo' class='img-section' />
+        <Image src={soda} alt='logo' class='logo' class='img-section' />
+        </section>
     </div>
   );
 };
