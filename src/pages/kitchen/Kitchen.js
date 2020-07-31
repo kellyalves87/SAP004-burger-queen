3764b2780d19f9cacc88681e792dcf039d1047d7
sjadriana <adriana.sj@live.com>
2020-07-31 19:02:26 -0300

    função pegar pedidos

--- src/pages/kitchen/Kitchen.js
+++ src/pages/kitchen/Kitchen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import firebase from "../../firebase-config";
 import {Link} from 'react-router-dom'
 import Button from '../../components/button/button';
@@ -6,10 +6,28 @@ import './Kitchen.css'
 import logo from "../../assets/logo.svg";
 import Image from '../../components/image/image'
 import exit from "../../assets/exit.svg";
+import "firebase/firebase-auth";
+import "firebase/firebase-firestore";
+import Order from "../../components/menu/order";
+import OrderItem from "../../components/menu/OrderItem";
 
 
 const Kitchen = () => {
+  const [orders, setOrders] = useState([])
+  useEffect(()=>{
+    firebase
+    .firestore()
+    .collection("orders")
+    .get().then((snapshot)=>{
+      const pedidos = snapshot.docs.map((doc) =>({
+        id: doc.id,
+        ...doc.data() 
+        //doc.data pega todos os itens dentro do pedido
+      })) 
+      setOrders(pedidos)
+    })
 
+  },[])
 
 
   return (
@@ -32,14 +50,25 @@ const Kitchen = () => {
       <section className='section-kitchen'>
         <div className='div-orderRecived'>
         <h1 className='h1-orders'>PEDIDOS PENDENTES</h1>
+        <div>
+          {orders.map((item)=> (
+            <div>
+              {item.name}
+              {(item.order).map((i)=>(
+                <div>{i.count}
+                {i.item}
+                 </div>
+              ))}
+            </div>
+          ))}
+
+        </div>
         </div>
         <div className='div-orderFinished'>
          <h1 className='h1-orders'>PEDIDOS PRONTOS</h1>
         </div>
       </section>
-      
-    
-    </div> 
+      </div> 
   );
 };
 
