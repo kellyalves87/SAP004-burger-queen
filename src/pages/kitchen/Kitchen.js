456bd18ad2465747025a0863f130968ecf4a112b
Kelly Alves <kellyp.alves87@gmail.com>
2020-08-06 16:25:10 -0300

    resolvendo conflitos

--- src/pages/kitchen/Kitchen.js
+++ src/pages/kitchen/Kitchen.js
@@ -1,52 +1,40 @@
 import React, { useState, useEffect } from "react";
 import firebase from "../../firebase-config";
-import { Link } from 'react-router-dom'
-import Button from '../../components/button/button';
-import './Kitchen.css'
-import logo from "../../assets/logo.svg";
-import Image from '../../components/image/image'
-import exit from "../../assets/exit.svg";
 import "firebase/firebase-auth";
 import "firebase/firebase-firestore";
-import growl from "growl-alert";
-import "growl-alert/dist/growl-alert.css";
+import { Link } from "react-router-dom";
 import Button from "../../components/button/button";
 import logo from "../../assets/logo.svg";
-import line from "../../assets/line.svg";
 import Image from "../../components/image/image";
 import exit from "../../assets/exit.svg";
-import OrderHistory from "../../components/menu/OrderHistory";
-import "./Kitchen.css";
 // import Order from "../../components/menu/order";
 // import OrderItem from "../../components/menu/OrderItem";
+import OrderHistory from "../../components/menu/OrderHistory";
+import "./Kitchen.css";
 
 const Kitchen = () => {
   const [done, setDone] = useState([]);
   const [pending, setPending] = useState([]);
   // const [orders, setOrders] = useState([]);
 
-    const option = {
-    fadeAway: true,
-    fadeAwayTimeout: 2000,
-  };
-
   useEffect(() => {
     firebase
       .firestore()
       .collection("orders")
       .orderBy("created_at", "desc")
-      .get().then((snapshot) => {
+      .get()
+      .then((snapshot) => {
         const pedidos = snapshot.docs.map((doc) => ({
           id: doc.id,
-          ...doc.data()
+          ...doc.data(),
           //doc.data pega todos os itens dentro do pedido
-        }))
+        }));
         // setOrders(pedidos)
 
-        setPending(pedidos.filter(doc => doc.ready === "pending"))
-        setDone(pedidos.filter(doc => doc.ready === "done"))
-      })
-  }, [])
+        setPending(pedidos.filter((doc) => doc.ready === "pending"));
+        setDone(pedidos.filter((doc) => doc.ready === "done"));
+      });
+  }, []);
 
   function orderDone(item) {
     firebase
@@ -57,103 +45,74 @@ const Kitchen = () => {
       .update({
         ready: "done",
         updated_at: new Date(),
-      })
-    console.log('foi')
+      });
+    console.log("foi");
 
     const newPending = pending.filter((el) => el.id !== item.id);
     setPending(newPending);
 
-    const newDone = [...done, { ...item, ready: 'done', updated_at: new Date() }];
+    const newDone = [
+      ...done,
+      { ...item, ready: "done", updated_at: new Date() },
+    ];
     setDone(newDone);
-  };
-  
-  function orderDone(item){
-    firebase
-    .firestore()
-    .collection("orders")
-    .doc(item.id)
-    .update({
-      ready: "done",
-      updated_at: new Date(),
-    })
-    console.log('foi')
+  }
 
-    const newPending = pending.filter((el) => el.id !== item.id);
-    setPending(newPending);
-
-
-    const newDone = [...done, {...item, ready: 'done', updated_at: new Date().getTime()}];
-    setDone(newDone);
-
-    growl.success({text: 'Pedido pronto para entrega!', ...option})
-};
-
-  // function orderHistory(item) {}
   return (
     <div className='div-kitchen'>
       <nav className='nav-kitchen'>
         <figure className='figure-kitchen'>
           <Image src={logo} alt='logo' class='logo-kitchen' />
         </figure>
-      
-        </Link>
-        <figure className='figure-line'>
-            <Image src={line} alt='line' class='line-kitchen' />
-          </figure>
         <h1 className='h1-kitchen'>BURGER QUEEN</h1>
-        <button
-          className='button-exit'
-          name='EXIT'
-          onClick={() => firebase.auth().signOut()}
-        >
-          <Image src={exit} alt='exit' class='exit-image' />
-        </button>
+        <Link to='/login'>
+          <button
+            className='button-exit'
+            name='EXIT'
+            onClick={() => firebase.auth().signOut()}
+          >
+            <Image src={exit} alt='exit' class='exit-image' />
+          </button>
+        </Link>
       </nav>
       <section className='section-kitchen'>
         <div className='div-orderRecived'>
           <h1 className='h1-orders'>PEDIDOS PENDENTES</h1>
-          <div >
-            {pending.map((item) =>
+          <div>
+            {pending.map((item) => (
               <div key={item.id}>
                 <OrderHistory
                   table={item.table}
                   name={item.name}
                   order={item.order.map((i) => (
-                    <div>{i.count}
+                    <div>
+                      {i.count}
                       {i.item}
-                    </div>))}
+                    </div>
+                  ))}
                 />
                 <Button
                   name='PRONTO'
                   onClick={(e) => {
-                    orderDone(item)
-                    e.preventDefault()
+                    orderDone(item);
+                    e.preventDefault();
                   }}
-                  title={'Pedido Pronto'}
+                  title={"Pedido Pronto"}
                 />
               </div>
-            )}
+            ))}
           </div>
         </div>
         <div className='div-orderFinished'>
           <h1 className='h1-orders'>PEDIDOS PRONTOS</h1>
           <div>
-
             <div>
-              {done.map((item) =>
-                <div key={item.id} >
+              {done.map((item) => (
+                <div key={item.id}>
                   <OrderHistory
                     table={item.table}
                     name={item.name}
                     order={item.order.map((i) => (
-
-                      <div>{i.count}
-                        {i.item}
-                      </div>))}
-                  />
-                </div>
-              )}
-
                       <div>
                         {i.count}
                         {i.item}
@@ -162,7 +121,6 @@ const Kitchen = () => {
                   />
                 </div>
               ))}
-
             </div>
           </div>
         </div>
@@ -170,4 +128,5 @@ const Kitchen = () => {
     </div>
   );
 };
+
 export default Kitchen;
